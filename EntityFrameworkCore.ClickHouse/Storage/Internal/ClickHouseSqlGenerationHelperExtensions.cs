e21a4c38831d512ca0c5d46774a7a4f04401313d
Denis Ivanov <denisvictorovichivanov@gmail.com>
2021-08-29 17:54:16 +0400

    Added ALTER TABLE UPDATE

---
+++ EntityFrameworkCore.ClickHouse/Storage/Internal/ClickHouseSqlGenerationHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Text;
+
+namespace ClickHouse.EntityFrameworkCore.Storage.Internal
+{
+    internal static class ClickHouseSqlGenerationHelperExtensions
+    {
+        internal static void GenerateParameterNamePlaceholder(
+            this ISqlGenerationHelper helper,
+            StringBuilder stringBuilder,
+            string name,
+            string type)
+        {
+            ((ClickHouseSqlGenerationHelper)helper).GenerateParameterNamePlaceholder(stringBuilder, name, type);
+        }
+    }
+}
