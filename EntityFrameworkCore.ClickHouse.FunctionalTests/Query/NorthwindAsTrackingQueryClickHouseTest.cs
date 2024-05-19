368cb712f52979e17ad68470d7fd48a750d689dd
Denis Ivanov <di@corpmail.co>
2024-05-19 13:10:19 +0300

    Added NorthwindAsTrackingQueryClickHouseTest

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindAsTrackingQueryClickHouseTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query;
+
+public class NorthwindAsTrackingQueryClickHouseTest : NorthwindAsTrackingQueryTestBase<NorthwindQueryClickHouseFixture<NoopModelCustomizer>>
+{
+    public NorthwindAsTrackingQueryClickHouseTest(NorthwindQueryClickHouseFixture<NoopModelCustomizer> fixture)
+        : base(fixture)
+    {
+    }
+}
