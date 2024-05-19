58e27cbb748e4549f126ab64f7c67da03c20b895
Denis Ivanov <di@corpmail.co>
2024-05-19 13:29:36 +0300

    Added NorthwindCompiledQueryClickHouseTest

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindCompiledQueryClickHouseTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query;
+
+public class NorthwindCompiledQueryClickHouseTest : NorthwindCompiledQueryTestBase<NorthwindQueryClickHouseFixture<NoopModelCustomizer>>
+{
+    public NorthwindCompiledQueryClickHouseTest(NorthwindQueryClickHouseFixture<NoopModelCustomizer> fixture)
+        : base(fixture)
+    {
+    }
+
+    [ConditionalFact(Skip = "TBD")]
+    public override void Keyless_query()
+    {
+        base.Keyless_query();
+    }
+
+    [ConditionalFact(Skip = "TBD")]
+    public override Task Keyless_query_async()
+    {
+        return base.Keyless_query_async();
+    }
+
+    [ConditionalFact(Skip = "TBD")]
+    public override void Keyless_query_first()
+    {
+        base.Keyless_query_first();
+    }
+
+    [ConditionalFact(Skip = "TBD")]
+    public override Task Keyless_query_first_async()
+    {
+        return base.Keyless_query_first_async();
+    }
+
+    [ConditionalFact(Skip = "TBD")]
+    public override void Query_with_array_parameter()
+    {
+        base.Query_with_array_parameter();
+    }
+
+    [ConditionalFact(Skip = "TBD")]
+    public override Task Query_with_array_parameter_async()
+    {
+        return base.Query_with_array_parameter_async();
+    }
+}
