485b266503cddcb0dba6a1c034678183e160c2bc
Denis Ivanov <di@corpmail.co>
2024-05-19 13:19:17 +0300

    Added NorthwindChangeTrackingQueryClickHouseTest

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindChangeTrackingQueryClickHouseTest.cs
@@ -0,0 +1,28 @@
+﻿using EntityFrameworkCore.ClickHouse.FunctionalTests.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query;
+
+public class NorthwindChangeTrackingQueryClickHouseTest : NorthwindChangeTrackingQueryTestBase<
+    NorthwindQueryClickHouseFixture<NoopModelCustomizer>>
+{
+    public NorthwindChangeTrackingQueryClickHouseTest(NorthwindQueryClickHouseFixture<NoopModelCustomizer> fixture)
+        : base(fixture)
+    {
+    }
+
+    protected override NorthwindContext CreateNoTrackingContext()
+        => new NorthwindClickHouseContext(
+            new DbContextOptionsBuilder(Fixture.CreateOptions())
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).Options);
+
+    [ConditionalFact(Skip = "TBD")]
+    public override void Multiple_entities_can_revert()
+    {
+        base.Multiple_entities_can_revert();
+    }
+}
