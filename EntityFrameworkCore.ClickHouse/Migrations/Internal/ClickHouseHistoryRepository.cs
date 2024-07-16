b4d7f717fe2db72695de0a748b6ec50305ffd254
Denis Ivanov <di@corpmail.co>
2024-07-16 23:09:15 +0300

    Add rename table support migrations operation

--- EntityFrameworkCore.ClickHouse/Migrations/Internal/ClickHouseHistoryRepository.cs
+++ EntityFrameworkCore.ClickHouse/Migrations/Internal/ClickHouseHistoryRepository.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.IO;
+using System.Text;
 
 namespace ClickHouse.EntityFrameworkCore.Migrations.Internal;
 
@@ -9,29 +13,74 @@ public class ClickHouseHistoryRepository : HistoryRepository
     }
 
     protected override bool InterpretExistsResult(object value)
-    {
-        throw new System.NotImplementedException();
-    }
+        => value != null && value != DBNull.Value && (byte)value == 1;
 
     public override string GetCreateIfNotExistsScript()
     {
-        throw new System.NotImplementedException();
+        var builder = new StringBuilder();
+        var isFirstLine = true;
+
+        using (var reader = new StringReader(GetCreateScript()))
+        {
+            while (reader.ReadLine() is { } line)
+            {
+                if (!isFirstLine)
+                {
+                    builder.AppendLine();
+                }
+                isFirstLine = false;
+
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    builder.Append("    ").Append(line);
+                }
+            }
+        }
+
+        return builder.ToString();
     }
 
     public override string GetBeginIfNotExistsScript(string migrationId)
     {
-        throw new System.NotImplementedException();
+        var stringTypeMapping = Dependencies.TypeMappingSource.GetMapping(typeof(string));
+
+        return new StringBuilder()
+            .AppendLine("IF NOT EXISTS (")
+            .Append("    SELECT * FROM ")
+            .AppendLine(SqlGenerationHelper.DelimitIdentifier(TableName, TableSchema))
+            .Append("    WHERE ")
+            .Append(SqlGenerationHelper.DelimitIdentifier(MigrationIdColumnName))
+            .Append(" = ").AppendLine(stringTypeMapping.GenerateSqlLiteral(migrationId))
+            .AppendLine(")")
+            .Append("BEGIN")
+            .ToString();
     }
 
     public override string GetBeginIfExistsScript(string migrationId)
     {
-        throw new System.NotImplementedException();
+        var stringTypeMapping = Dependencies.TypeMappingSource.GetMapping(typeof(string));
+
+        return new StringBuilder()
+            .AppendLine("IF EXISTS (")
+            .Append("    SELECT * FROM ")
+            .AppendLine(SqlGenerationHelper.DelimitIdentifier(TableName, TableSchema))
+            .Append("    WHERE ")
+            .Append(SqlGenerationHelper.DelimitIdentifier(MigrationIdColumnName))
+            .Append(" = ")
+            .AppendLine(stringTypeMapping.GenerateSqlLiteral(migrationId))
+            .AppendLine(")")
+            .Append("BEGIN")
+            .ToString();
     }
 
     public override string GetEndIfScript()
-    {
-        throw new System.NotImplementedException();
-    }
+        => new StringBuilder()
+            .Append("END")
+            .AppendLine(SqlGenerationHelper.StatementTerminator)
+            .ToString();
 
-    protected override string ExistsSql { get; }
+    protected override string ExistsSql
+        => "EXISTS " +
+           SqlGenerationHelper.DelimitIdentifier(TableName, TableSchema)
+           + SqlGenerationHelper.StatementTerminator;
 }
