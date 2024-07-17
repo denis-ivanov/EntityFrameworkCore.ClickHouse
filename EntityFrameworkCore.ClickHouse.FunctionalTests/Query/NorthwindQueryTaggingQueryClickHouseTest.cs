2469c47a9de7afeb6b560673cf32740944f13113
Denis Ivanov <di@corpmail.co>
2024-07-17 21:00:06 +0300

    Add NorthwindQueryTaggingQueryClickHouseTest

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindQueryTaggingQueryClickHouseTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query;
+
+public class NorthwindQueryTaggingQueryClickHouseTest : NorthwindQueryTaggingQueryTestBase<NorthwindQueryClickHouseFixture<NoopModelCustomizer>>
+{
+    public NorthwindQueryTaggingQueryClickHouseTest(
+        NorthwindQueryClickHouseFixture<NoopModelCustomizer> fixture,
+        ITestOutputHelper testOutputHelper)
+        : base(fixture)
+    {
+    }
+}
