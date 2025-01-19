e26ffb7ef22898dbf89b7b14f28c6a0b27799b24
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-01-19 19:53:53 +0200

    Update packages

---
+++ EntityFrameworkCore.ClickHouse/Migrations/Internal/ClickHouseMigrationDatabaseLock.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClickHouse.EntityFrameworkCore.Migrations.Internal;
+
+public class ClickHouseMigrationDatabaseLock : IMigrationsDatabaseLock
+{
+    public ClickHouseMigrationDatabaseLock(IHistoryRepository historyRepository)
+    {
+        HistoryRepository = historyRepository;
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    public IHistoryRepository HistoryRepository { get; }
+}
