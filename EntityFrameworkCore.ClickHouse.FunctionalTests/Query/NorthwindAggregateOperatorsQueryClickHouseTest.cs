a2b7d8669451be1185050f1d0d0e68c191bcff17
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-11-15 20:27:10 +0200

    Enable Contains_inside_aggregate_function_with_GroupBy

--- EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindAggregateOperatorsQueryClickHouseTest.cs
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindAggregateOperatorsQueryClickHouseTest.cs
@@ -339,12 +339,6 @@ public class NorthwindAggregateOperatorsQueryClickHouseTest : NorthwindAggregate
     }
 
     [ConditionalTheory(Skip = "TBD"), MemberData(nameof(IsAsyncData))]
-    public override Task Contains_inside_aggregate_function_with_GroupBy(bool async)
-    {
-        return base.Contains_inside_aggregate_function_with_GroupBy(async);
-    }
-
-    [ConditionalTheory(Skip = "TBD"), MemberData(nameof(IsAsyncData))]
     public override Task Contains_with_local_collection_complex_predicate_and(bool async)
     {
         return base.Contains_with_local_collection_complex_predicate_and(async);
