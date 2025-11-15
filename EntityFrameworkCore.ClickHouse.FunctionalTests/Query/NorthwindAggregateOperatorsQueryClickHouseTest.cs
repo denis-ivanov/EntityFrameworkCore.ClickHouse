e4a716b5cf17151d0557c429f467bcdb74530aa0
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-11-15 20:29:27 +0200

    Enable Contains_with_local_collection_complex_predicate_and

--- EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindAggregateOperatorsQueryClickHouseTest.cs
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/NorthwindAggregateOperatorsQueryClickHouseTest.cs
@@ -339,12 +339,6 @@ public class NorthwindAggregateOperatorsQueryClickHouseTest : NorthwindAggregate
     }
 
     [ConditionalTheory(Skip = "TBD"), MemberData(nameof(IsAsyncData))]
-    public override Task Contains_with_local_collection_complex_predicate_and(bool async)
-    {
-        return base.Contains_with_local_collection_complex_predicate_and(async);
-    }
-
-    [ConditionalTheory(Skip = "TBD"), MemberData(nameof(IsAsyncData))]
     public override Task Contains_with_local_collection_complex_predicate_not_matching_ins1(bool async)
     {
         return base.Contains_with_local_collection_complex_predicate_not_matching_ins1(async);
