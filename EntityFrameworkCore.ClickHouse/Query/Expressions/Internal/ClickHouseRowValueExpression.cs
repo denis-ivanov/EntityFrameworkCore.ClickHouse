8a520e90d360e65d817d5cc2ca04e90e792964e1
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-11-16 22:34:02 +0200

    Copy PostgreSQL row comparison implementation

---
+++ EntityFrameworkCore.ClickHouse/Query/Expressions/Internal/ClickHouseRowValueExpression.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ClickHouse.EntityFrameworkCore.Query.Expressions.Internal;
+
+public class ClickHouseRowValueExpression : SqlExpression, IEquatable<ClickHouseRowValueExpression>
+{
+    private static ConstructorInfo? _quotingConstructor;
+
+    public ClickHouseRowValueExpression(IReadOnlyList<SqlExpression> values, Type type, RelationalTypeMapping? typeMapping = null)
+        : base(type, typeMapping)
+    {
+        Values = values;
+    }
+
+    public virtual IReadOnlyList<SqlExpression> Values { get; }
+
+    protected override Expression VisitChildren(ExpressionVisitor visitor)
+    {
+        SqlExpression[]? newRowValues = null;
+
+        for (var i = 0; i < Values.Count; i++)
+        {
+            var rowValue = Values[i];
+            var visited = (SqlExpression)visitor.Visit(rowValue);
+            if (visited != rowValue && newRowValues is null)
+            {
+                newRowValues = new SqlExpression[Values.Count];
+                for (var j = 0; j < i; j++)
+                {
+                    newRowValues[j] = Values[j];
+                }
+            }
+
+            if (newRowValues is not null)
+            {
+                newRowValues[i] = visited;
+            }
+        }
+
+        return newRowValues is null ? this : new ClickHouseRowValueExpression(newRowValues, Type);
+    }
+
+    public virtual ClickHouseRowValueExpression Update(IReadOnlyList<SqlExpression> values)
+        => values.Count == Values.Count && values.Zip(Values, (x, y) => (x, y)).All(tup => tup.x == tup.y)
+            ? this
+            : new ClickHouseRowValueExpression(values, Type);
+
+    public override Expression Quote()
+        => New(
+            _quotingConstructor ??= typeof(ClickHouseRowValueExpression).GetConstructor(
+                [typeof(IReadOnlyList<SqlExpression>), typeof(Type), typeof(RelationalTypeMapping)])!,
+            NewArrayInit(typeof(SqlExpression), initializers: Values.Select(a => a.Quote())),
+            Constant(Type),
+            RelationalExpressionQuotingUtilities.QuoteTypeMapping(TypeMapping));
+
+    protected override void Print(ExpressionPrinter expressionPrinter)
+    {
+        expressionPrinter.Append("(");
+
+        var count = Values.Count;
+        for (var i = 0; i < count; i++)
+        {
+            expressionPrinter.Visit(Values[i]);
+
+            if (i < count - 1)
+            {
+                expressionPrinter.Append(", ");
+            }
+        }
+
+        expressionPrinter.Append(")");
+    }
+
+    public override bool Equals(object? obj)
+        => obj is ClickHouseRowValueExpression other && Equals(other);
+
+    public virtual bool Equals(ClickHouseRowValueExpression? other)
+    {
+        if (other is null || !base.Equals(other) || other.Values.Count != Values.Count)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        for (var i = 0; i < Values.Count; i++)
+        {
+            if (!other.Values[i].Equals(Values[i]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+
+        foreach (var rowValue in Values)
+        {
+            hashCode.Add(rowValue);
+        }
+
+        return hashCode.ToHashCode();
+    }
+}
--- EntityFrameworkCore.ClickHouse/Query/Internal/ClickHouseSqlTranslatingExpressionVisitor.cs
+++ EntityFrameworkCore.ClickHouse/Query/Internal/ClickHouseSqlTranslatingExpressionVisitor.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using ClickHouse.EntityFrameworkCore.Query.Expressions.Internal;
+using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace ClickHouse.EntityFrameworkCore.Query.Internal;
 
@@ -24,6 +27,20 @@ public class ClickHouseSqlTranslatingExpressionVisitor : RelationalSqlTranslatin
     {
     }
 
+    public override SqlExpression GenerateLeast(IReadOnlyList<SqlExpression> expressions, Type resultType)
+    {
+        var resultTypeMapping = ExpressionExtensions.InferTypeMapping(expressions);
+
+        return Dependencies.SqlExpressionFactory.Function("least", expressions, nullable: true, Enumerable.Repeat(true, expressions.Count), resultType, resultTypeMapping);
+    }
+
+    public override SqlExpression GenerateGreatest(IReadOnlyList<SqlExpression> expressions, Type resultType)
+    {
+        var resultTypeMapping = ExpressionExtensions.InferTypeMapping(expressions);
+
+        return Dependencies.SqlExpressionFactory.Function("greatest", expressions, nullable: true, Enumerable.Repeat(true, expressions.Count), resultType, resultTypeMapping);
+    }
+    
     protected override Expression VisitMember(MemberExpression memberExpression)
     {
         if (IsDateDiffExpression(memberExpression, out var left, out var right, out var unit))
@@ -62,18 +79,40 @@ public class ClickHouseSqlTranslatingExpressionVisitor : RelationalSqlTranslatin
         return base.VisitBinary(binaryExpression);
     }
 
-    public override SqlExpression GenerateGreatest(IReadOnlyList<SqlExpression> expressions, Type resultType)
+    protected override Expression VisitNew(NewExpression newExpression)
     {
-        var resultTypeMapping = ExpressionExtensions.InferTypeMapping(expressions);
+        var visitedNewExpression = base.VisitNew(newExpression);
 
-        return Dependencies.SqlExpressionFactory.Function("greatest", expressions, nullable: true, Enumerable.Repeat(true, expressions.Count), resultType, resultTypeMapping);
-    }
+        if (visitedNewExpression != QueryCompilationContext.NotTranslatedExpression)
+        {
+            return visitedNewExpression;
+        }
 
-    public override SqlExpression GenerateLeast(IReadOnlyList<SqlExpression> expressions, Type resultType)
-    {
-        var resultTypeMapping = ExpressionExtensions.InferTypeMapping(expressions);
+        if (newExpression.Type.IsAssignableTo(typeof(ITuple)))
+        {
+            return TryTranslateArguments(out var sqlArguments)
+                ? new ClickHouseRowValueExpression(sqlArguments, newExpression.Type)
+                : QueryCompilationContext.NotTranslatedExpression;
+        }
+        
+        return QueryCompilationContext.NotTranslatedExpression;
+        
+        bool TryTranslateArguments(out SqlExpression[] sqlArguments)
+        {
+            sqlArguments = new SqlExpression[newExpression.Arguments.Count];
+            for (var i = 0; i < sqlArguments.Length; i++)
+            {
+                var argument = newExpression.Arguments[i];
+                if (TranslationFailed(argument, Visit(argument), out var sqlArgument))
+                {
+                    return false;
+                }
 
-        return Dependencies.SqlExpressionFactory.Function("least", expressions, nullable: true, Enumerable.Repeat(true, expressions.Count), resultType, resultTypeMapping);
+                sqlArguments[i] = sqlArgument!;
+            }
+
+            return true;
+        }
     }
 
     private static bool IsDateDiffExpression(MemberExpression memberExpression, out Expression left, out Expression right, out string unit)
@@ -102,5 +141,18 @@ public class ClickHouseSqlTranslatingExpressionVisitor : RelationalSqlTranslatin
         return type == typeof(DateOnly) ||
                type == typeof(DateTime) ||
                type == typeof(DateTimeOffset);
+    }
+    
+    [DebuggerStepThrough]
+    private static bool TranslationFailed(Expression original, Expression translation, out SqlExpression castTranslation)
+    {
+        if (original is not null && !(translation is SqlExpression))
+        {
+            castTranslation = null;
+            return true;
+        }
+
+        castTranslation = translation as SqlExpression;
+        return false;
     }
 }
