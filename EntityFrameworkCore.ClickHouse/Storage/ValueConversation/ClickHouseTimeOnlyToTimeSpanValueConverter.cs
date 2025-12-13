1e7116161a6f2b325eb3872ad094e828e784becb
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-14 01:51:31 +0200

    Add TimeOnly support

---
+++ EntityFrameworkCore.ClickHouse/Storage/ValueConversation/ClickHouseTimeOnlyToTimeSpanValueConverter.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+
+namespace ClickHouse.EntityFrameworkCore.Storage.ValueConversation;
+
+public class ClickHouseTimeOnlyToTimeSpanValueConverter : ValueConverter<TimeOnly, TimeSpan>
+{
+    public ClickHouseTimeOnlyToTimeSpanValueConverter()
+        : base(to => to.ToTimeSpan(), ts => TimeOnly.FromTimeSpan(ts))
+    {
+    }
+}
