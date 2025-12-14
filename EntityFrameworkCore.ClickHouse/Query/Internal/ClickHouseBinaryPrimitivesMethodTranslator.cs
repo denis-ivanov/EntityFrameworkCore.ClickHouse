1ff4f421b0e7fa67251231dbd3d31afe6a8fb1fa
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-14 07:29:02 +0200

    Add `byteSwap` function support

---
+++ EntityFrameworkCore.ClickHouse/Query/Internal/ClickHouseBinaryPrimitivesMethodTranslator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ClickHouse.EntityFrameworkCore.Query.Internal;
+
+public class ClickHouseBinaryPrimitivesMethodTranslator : IMethodCallTranslator
+{
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+
+    public ClickHouseBinaryPrimitivesMethodTranslator(
+        ISqlExpressionFactory sqlExpressionFactory,
+        IRelationalTypeMappingSource typeMappingSource)
+    {
+        _sqlExpressionFactory = sqlExpressionFactory;
+        _typeMappingSource = typeMappingSource;
+    }
+
+    public SqlExpression Translate(
+        SqlExpression instance,
+        MethodInfo method,
+        IReadOnlyList<SqlExpression> arguments,
+        IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+    {
+        if (method.Name == nameof(BinaryPrimitives.ReverseEndianness) && arguments.Count == 1)
+        {
+            return _sqlExpressionFactory.Function(
+                name: "byteSwap",
+                arguments: arguments,
+                argumentsPropagateNullability: [true],
+                returnType: method.ReturnType,
+                nullable: true,
+                typeMapping: _typeMappingSource.FindMapping(method.ReturnType));
+        }
+
+        return null;
+    }
+}
