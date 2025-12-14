004b9fab3bb2a8ac3f5751ef88ab7e26005f6111
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-14 08:04:52 +0200

    Add `divide` function support

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Query/ArithmeticDbFunctionsExtensionsClickHouseTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query;
+
+public class ArithmeticDbFunctionsExtensionsClickHouseTest : IClassFixture<NorthwindQueryClickHouseFixture<NoopModelCustomizer>>
+{
+    public ArithmeticDbFunctionsExtensionsClickHouseTest(
+        NorthwindQueryClickHouseFixture<NoopModelCustomizer> fixture,
+        ITestOutputHelper testOutputHelper)
+    {
+        Fixture = fixture;
+        Fixture.TestSqlLoggerFactory.Clear();
+        Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+    }
+
+    [Fact]
+    public async Task Divide()
+    {
+        var context = CreateContext();
+        
+        await context.OrderDetails
+            .Where(e => e.OrderID == 10285)
+            .Select(e => new { Double = EF.Functions.Divide(e.UnitPrice, e.Discount) })
+            .ToListAsync();
+
+        AssertSql(
+            """
+            SELECT divide("o"."UnitPrice", "o"."Discount") AS "Double"
+            FROM "OrderDetails" AS "o"
+            WHERE "o"."OrderID" = 10285
+            """);
+    }
+
+    protected NorthwindQueryClickHouseFixture<NoopModelCustomizer> Fixture { get; }
+    
+    private void AssertSql(params string[] expected)
+        => Fixture.TestSqlLoggerFactory.AssertBaseline(expected);
+
+    protected NorthwindContext CreateContext()
+        => Fixture.CreateContext();
+}
---
+++ EntityFrameworkCore.ClickHouse/Query/Internal/ClickHouseArithmeticMethodTranslator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ClickHouse.EntityFrameworkCore.Query.Internal;
+
+public class ClickHouseArithmeticMethodTranslator : IMethodCallTranslator
+{
+    private readonly ISqlExpressionFactory _sqlExpressionFactory;
+    private readonly IRelationalTypeMappingSource _typeMappingSource;
+
+    public ClickHouseArithmeticMethodTranslator(
+        ISqlExpressionFactory sqlExpressionFactory,
+        IRelationalTypeMappingSource typeMappingSource)
+    {
+        _sqlExpressionFactory = sqlExpressionFactory;
+        _typeMappingSource = typeMappingSource;
+    }
+
+    public SqlExpression Translate(
+        SqlExpression instance,
+        MethodInfo method,
+        IReadOnlyList<SqlExpression> arguments,
+        IDiagnosticsLogger<DbLoggerCategory.Query> logger)
+    {
+        if (method.DeclaringType != typeof(ClickHouseArithmeticDbFunctionsExtensions))
+        {
+            return null;
+        }
+
+        return method.Name switch
+        {
+            nameof(ClickHouseArithmeticDbFunctionsExtensions.Divide) => _sqlExpressionFactory.Function(
+                name: "divide",
+                arguments: [arguments[1], arguments[2]],
+                nullable: true,
+                argumentsPropagateNullability: [true, true],
+                returnType: typeof(double),
+                typeMapping: _typeMappingSource.FindMapping(typeof(double))),
+            _ => null
+        };
+    }
+}
