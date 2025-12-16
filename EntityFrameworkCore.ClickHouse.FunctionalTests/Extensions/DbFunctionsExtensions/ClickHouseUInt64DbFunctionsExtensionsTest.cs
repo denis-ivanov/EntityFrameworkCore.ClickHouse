c64911cdf3f0d9f2272d8ae189b837d55dd4d3da
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-16 23:46:03 +0200

    Add `ToUInt64` tests

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Extensions/DbFunctionsExtensions/ClickHouseUInt64DbFunctionsExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Extensions.DbFunctionsExtensions;
+
+public sealed class ClickHouseUInt64DbFunctionsExtensionsTest
+{
+    [Fact]
+    public void ToUInt64_Int32_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt64(123));
+    }
+
+    [Fact]
+    public void ToUInt64_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt64("123"));
+    }
+
+    [Fact]
+    public void ToUInt64OrZero_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt64OrZero("123"));
+    }
+
+    [Fact]
+    public void ToUInt64OrNull_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt64OrNull("123"));
+    }
+
+    [Fact]
+    public void ToUInt64OrDefault_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt64OrDefault("123"));
+    }
+
+    [Fact]
+    public void ToUInt64OrDefault_StringWithDefault_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt64OrDefault("123", 42ul));
+    }
+}
