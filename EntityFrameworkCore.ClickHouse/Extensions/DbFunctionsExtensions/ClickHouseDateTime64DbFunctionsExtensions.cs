2db16a1b03b789c06667aad0be6edebad281f4b9
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-16 20:46:46 +0200

    Add ToDateTime64 tests

--- EntityFrameworkCore.ClickHouse/Extensions/DbFunctionsExtensions/ClickHouseDateTime64DbFunctionsExtensions.cs
+++ EntityFrameworkCore.ClickHouse/Extensions/DbFunctionsExtensions/ClickHouseDateTime64DbFunctionsExtensions.cs
@@ -6,41 +6,133 @@ namespace Microsoft.EntityFrameworkCore;
 
 public static class ClickHouseDateTime64DbFunctionsExtensions
 {
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, string expr, [Range(0, 9)] byte scale)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <param name="timeZone">Time zone of the specified datetime64 object.</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, string expr, [Range(0, 9)] byte scale, string timeZone)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, uint expr, [Range(0, 9)] byte scale)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <param name="timeZone">Time zone of the specified datetime64 object.</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, uint expr, [Range(0, 9)] byte scale, string timeZone)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, DateOnly expr, [Range(0, 9)] byte scale)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <param name="timeZone">Time zone of the specified datetime64 object.</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, DateOnly expr, [Range(0, 9)] byte scale, string timeZone)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, float expr, [Range(0, 9)] byte scale)
     {
         throw new InvalidOperationException();
     }
 
+    /// <summary>
+    /// Converts an input value to a value of type DateTime64.
+    /// </summary>
+    /// <param name="_">DbFunctions instance.</param>
+    /// <param name="expr">The value.</param>
+    /// <param name="scale">Tick size (precision).</param>
+    /// <param name="timeZone">Time zone of the specified datetime64 object.</param>
+    /// <returns>A calendar date and time of day, with sub-second precision.</returns>
+    /// <remarks>https://clickhouse.com/docs/sql-reference/functions/type-conversion-functions#todatetime64</remarks>
+    /// <exception cref="InvalidOperationException">
+    /// <see cref="ToDateTime64"/> is only intended for use via SQL translation as part of an EF Core LINQ query.
+    /// </exception>
     public static DateTime ToDateTime64(this DbFunctions _, float expr, [Range(0, 9)] byte scale, string timeZone)
     {
         throw new InvalidOperationException();
