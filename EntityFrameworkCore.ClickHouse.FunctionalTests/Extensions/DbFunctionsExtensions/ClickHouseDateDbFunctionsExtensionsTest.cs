cd5b44f10f9e8bc017aaa147d589d0bbe8ae6e73
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-16 22:52:56 +0200

    Add `toDate*` tests and documentation

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Extensions/DbFunctionsExtensions/ClickHouseDateDbFunctionsExtensionsTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Extensions.DbFunctionsExtensions;
+
+public sealed class ClickHouseDateDbFunctionsExtensionsTest
+{
+    [Fact]
+    public void ToDate_Int32_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDate(123));
+    } 
+    
+    [Fact]
+    public void ToDate_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDate("2025-12-16"));
+    }
+    
+    [Fact]
+    public void ToDate_DateTime_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDate(DateTime.Now));
+    }
+
+    [Fact]
+    public void ToDate_Int32WithTimeZone_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDate(123, "Asia/Istanbul"));
+    }
+
+    [Fact]
+    public void ToDate_StringWithTimeZone_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDate("2025-12-16", "Asia/Istanbul"));
+    }
+
+    [Fact]
+    public void ToDate_DateTimeWithTimeZone_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDate(DateTime.Now, "Asia/Istanbul"));
+    }
+
+    [Fact]
+    public void ToDateOrZero_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDateOrZero("2025-12-16"));
+    }
+
+    [Fact]
+    public void ToDateOrNull_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDateOrNull("2025-12-16"));
+    }
+
+    [Fact]
+    public void ToDateOrDefault_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDateOrDefault("2025-12-16"));
+    }
+
+    [Fact]
+    public void ToDateOrDefault_StringWithDefault_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToDateOrDefault("2025-12-16", new DateOnly(2025, 12, 16)));
+    }
+}
