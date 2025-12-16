a64e828b6b5b2b7d92d2b7e62ab23194d90d1cfe
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-16 23:36:47 +0200

    Add `ToUInt32` tests

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Extensions/DbFunctionsExtensions/ClickHouseUInt32DbFunctionsExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Extensions.DbFunctionsExtensions;
+
+public sealed class ClickHouseUInt32DbFunctionsExtensionsTest
+{
+    [Fact]
+    public void ToUInt32_Int32_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt32(123));
+    }
+
+    [Fact]
+    public void ToUInt32_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt32("123"));
+    }
+
+    [Fact]
+    public void ToUInt32OrZero_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt32OrZero("123"));
+    }
+
+    [Fact]
+    public void ToUInt32OrNull_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt32OrNull("123"));
+    }
+
+    [Fact]
+    public void ToUInt32OrDefault_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt32OrDefault("123"));
+    }
+
+    [Fact]
+    public void ToUInt32OrDefault_StringWithDefault_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt32OrDefault("123", 42u));
+    }
+}
