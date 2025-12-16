7e5712f90ed24689101433921ed3823605e0fd6b
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-17 00:05:06 +0200

    Add `toUInt8*`, `toUInt16` tests

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Extensions/DbFunctionsExtensions/ClickHouseUInt8DbFunctionsExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Extensions.DbFunctionsExtensions;
+
+public sealed class ClickHouseUInt8DbFunctionsExtensionsTest
+{
+    [Fact]
+    public void ToUInt8_Int32_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt8(123));
+    }
+
+    [Fact]
+    public void ToUInt8_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt8("123"));
+    }
+
+    [Fact]
+    public void ToUInt8OrZero_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt8OrZero("123"));
+    }
+
+    [Fact]
+    public void ToUInt8OrNull_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt8OrNull("123"));
+    }
+
+    [Fact]
+    public void ToUInt8OrDefault_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt8OrDefault("123"));
+    }
+
+    [Fact]
+    public void ToUInt8OrDefault_StringWithDefault_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToUInt8OrDefault("123", (byte)42));
+    }
+}
