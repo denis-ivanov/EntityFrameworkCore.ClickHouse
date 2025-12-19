ec46da055cbd04dea3b7f3d301e9acfb34652e31
Denis Ivanov <denisvictorovichivanov@gmail.com>
2025-12-19 13:30:11 +0200

    Add `toFloat64*` tests and documentation

---
+++ EntityFrameworkCore.ClickHouse.FunctionalTests/Extensions/DbFunctionsExtensions/ClickHouseFloat64DbFunctionsExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Xunit;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Extensions.DbFunctionsExtensions;
+
+public sealed class ClickHouseFloat64DbFunctionsExtensionsTest
+{
+    [Fact]
+    public void ToFloat64_Int32_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToFloat64(123));
+    }
+
+    [Fact]
+    public void ToFloat64_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToFloat64("123.45"));
+    }
+
+    [Fact]
+    public void ToFloat64OrZero_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToFloat64OrZero("123.45"));
+    }
+
+    [Fact]
+    public void ToFloat64OrNull_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToFloat64OrNull("123.45"));
+    }
+
+    [Fact]
+    public void ToFloat64OrDefault_String_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToFloat64OrDefault("123.45"));
+    }
+
+    [Fact]
+    public void ToFloat64OrDefault_StringWithDefault_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => EF.Functions.ToFloat64OrDefault("123.45", 42.0));
+    }
+}
