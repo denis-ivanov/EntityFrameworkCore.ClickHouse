9f3cb0148118c755397cf6c069283f18ad93b65f
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-02-02 21:28:04 +0200

    Add `MathTranslationsClickHouseTest`

---
+++ test/EntityFrameworkCore.ClickHouse.FunctionalTests/Query/Translations/MathTranslationsClickHouseTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Translations;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query.Translations;
+
+public class MathTranslationsClickHouseTest : MathTranslationsTestBase<BasicTypesQueryClickHouseFixture>
+{
+    public MathTranslationsClickHouseTest(BasicTypesQueryClickHouseFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture)
+    {
+        Fixture.TestSqlLoggerFactory.Clear();
+        Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+    }
+}
