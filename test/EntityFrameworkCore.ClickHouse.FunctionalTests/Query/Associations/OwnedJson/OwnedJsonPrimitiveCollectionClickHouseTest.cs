8b2a9bc378f611492843ece31c3d70f5199e5493
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-03-26 23:24:54 +0200

    Add OwnedJson directory tests

---
+++ test/EntityFrameworkCore.ClickHouse.FunctionalTests/Query/Associations/OwnedJson/OwnedJsonPrimitiveCollectionClickHouseTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Associations.OwnedJson;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query.Associations.OwnedJson;
+
+public class OwnedJsonPrimitiveCollectionClickHouseTest : OwnedJsonPrimitiveCollectionRelationalTestBase<OwnedJsonClickHouseFixture>
+{
+    public OwnedJsonPrimitiveCollectionClickHouseTest(OwnedJsonClickHouseFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture, testOutputHelper)
+    {
+    }
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Count() => base.Count();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Select_Sum() => base.Select_Sum();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Any_predicate() => base.Any_predicate();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Nested_Count() => base.Nested_Count();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Index() => base.Index();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Contains() => base.Contains();
+}
