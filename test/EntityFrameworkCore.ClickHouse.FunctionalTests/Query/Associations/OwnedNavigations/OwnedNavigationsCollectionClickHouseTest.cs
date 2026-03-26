d469d7b0f6ead95f9179a42b631d5852ed0f1ffc
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-03-26 23:39:30 +0200

    Add OwnedNavigations directory tests

---
+++ test/EntityFrameworkCore.ClickHouse.FunctionalTests/Query/Associations/OwnedNavigations/OwnedNavigationsCollectionClickHouseTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Associations.OwnedNavigations;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query.Associations.OwnedNavigations;
+
+public class OwnedNavigationsCollectionClickHouseTest : OwnedNavigationsCollectionRelationalTestBase<OwnedNavigationsClickHouseFixture>
+{
+    public OwnedNavigationsCollectionClickHouseTest(OwnedNavigationsClickHouseFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture, testOutputHelper)
+    {
+    }
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task OrderBy_ElementAt() => base.OrderBy_ElementAt();
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Distinct_projected(QueryTrackingBehavior queryTrackingBehavior) => base.Distinct_projected(queryTrackingBehavior);
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Count() => base.Count();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Where() => base.Where();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Distinct() => base.Distinct();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task GroupBy() => base.GroupBy();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Select_within_Select_within_Select_with_aggregates() => base.Select_within_Select_within_Select_with_aggregates();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Index_column() => base.Index_column();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Distinct_over_projected_filtered_nested_collection() => base.Distinct_over_projected_filtered_nested_collection();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Index_parameter() => base.Index_parameter();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Index_constant() => base.Index_constant();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Index_out_of_bounds() => base.Index_out_of_bounds();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Index_on_nested_collection() => base.Index_on_nested_collection();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Distinct_over_projected_nested_collection() => base.Distinct_over_projected_nested_collection();
+}
---
+++ test/EntityFrameworkCore.ClickHouse.FunctionalTests/Query/Associations/OwnedNavigations/OwnedNavigationsSetOperationsClickHouseTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Associations.OwnedNavigations;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query.Associations.OwnedNavigations;
+
+public class OwnedNavigationsSetOperationsClickHouseTest : OwnedNavigationsSetOperationsRelationalTestBase<OwnedNavigationsClickHouseFixture>
+{
+    public OwnedNavigationsSetOperationsClickHouseTest(OwnedNavigationsClickHouseFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture, testOutputHelper)
+    {
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Over_assocate_collection_Select_nested_with_aggregates_projected(QueryTrackingBehavior queryTrackingBehavior) => base.Over_assocate_collection_Select_nested_with_aggregates_projected(queryTrackingBehavior);
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Over_associate_collection_projected(QueryTrackingBehavior queryTrackingBehavior) => base.Over_associate_collection_projected(queryTrackingBehavior);
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Over_nested_associate_collection() => base.Over_nested_associate_collection();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Over_different_collection_properties() => base.Over_different_collection_properties();
+
+    [ConditionalFact(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Over_associate_collections() => base.Over_associate_collections();
+}
