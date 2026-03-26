6136993450bcc78a5f15421165b0993a468a0084
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-03-26 04:15:28 +0200

    Add ComplexJson directory tests

---
+++ test/EntityFrameworkCore.ClickHouse.FunctionalTests/Query/Associations/ComplexJson/ComplexJsonProjectionClickHouseTest.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Associations.ComplexJson;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace EntityFrameworkCore.ClickHouse.FunctionalTests.Query.Associations.ComplexJson;
+
+public class ComplexJsonProjectionClickHouseTest : ComplexJsonProjectionRelationalTestBase<ComplexJsonClickHouseFixture>
+{
+    public ComplexJsonProjectionClickHouseTest(ComplexJsonClickHouseFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture, testOutputHelper)
+    {
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_associate_collection(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_associate_collection(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_nested_collection_on_optional_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_nested_collection_on_optional_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_nested_collection_on_required_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_nested_collection_on_required_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_nullable_value_type_property_on_null_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_nullable_value_type_property_on_null_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_optional_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_optional_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_optional_nested_on_optional_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_optional_nested_on_optional_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_optional_nested_on_required_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_optional_nested_on_required_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_property_on_optional_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_property_on_optional_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_required_associate_via_optional_navigation(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_required_associate_via_optional_navigation(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_required_nested_on_optional_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_required_nested_on_optional_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_required_nested_on_required_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_required_nested_on_required_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_root(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_root(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override Task Select_root_duplicated(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        return base.Select_root_duplicated(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_scalar_property_on_required_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_scalar_property_on_required_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_subquery_optional_related_FirstOrDefault(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_subquery_optional_related_FirstOrDefault(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_subquery_required_related_FirstOrDefault(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_subquery_required_related_FirstOrDefault(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_unmapped_associate_scalar_property(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_unmapped_associate_scalar_property(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_untranslatable_method_on_associate_scalar_property(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_untranslatable_method_on_associate_scalar_property(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_value_type_property_on_null_associate_throws(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_value_type_property_on_null_associate_throws(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task SelectMany_associate_collection(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.SelectMany_associate_collection(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task SelectMany_nested_collection_on_optional_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.SelectMany_nested_collection_on_optional_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_non_nullable_value_type(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_non_nullable_value_type(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task SelectMany_nested_collection_on_required_associate(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.SelectMany_nested_collection_on_required_associate(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_nullable_value_type(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_nullable_value_type(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_nullable_value_type_with_Value(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_nullable_value_type_with_Value(queryTrackingBehavior);
+    }
+
+    [ConditionalTheory(Skip = ClickHouseSkipReasons.Tbd)]
+    public override async Task Select_root_with_value_types(QueryTrackingBehavior queryTrackingBehavior)
+    {
+        await base.Select_root_with_value_types(queryTrackingBehavior);
+    }
+}
